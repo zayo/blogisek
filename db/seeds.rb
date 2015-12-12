aac24565ba957fbc39c611bb0de9a86304d7ec90
Stanislav Nedbálek <stanislav.nedbalek@gmail.com>
2015-12-12 21:08:45 +0100

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.create!([{ email: "admin@admin.com", password: "adminadmin", reset_password_token: nil, unconfirmed_email: nil, confirmed_at: "2015-12-12 17:19:17" }]);
+admin = User.create!({ email: "admin@admin.com", password: "adminadmin", reset_password_token: nil, unconfirmed_email: nil, confirmed_at: "2015-12-12 17:19:17" });
 admin.add_role :admin
 
 =begin
@@ -14,8 +14,3 @@ User.first_or_create([{ email: 'kokot1@velky.com', password: 'kokotiny' },{ emai
 Post.create({ user_id: user1.id, title: 'Nazev1', description: 'simple test of post' })
 Post.create({ user_id: user2.id, title: 'Nazev2', description: 'simple test of post' })
 =end
-
-
-Role.create!([
-  {name: "admin", resource_id: nil, resource_type: nil}
-])
