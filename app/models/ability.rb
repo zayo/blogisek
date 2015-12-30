a462ca2cf27ecefe8fc57ad1733195ff984ca9f9
Stanislav Nedbálek <stanislav.nedbalek@gmail.com>
2015-12-30 21:56:53 +0100

    readme

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
       can [:update, :destroy], Post, :user_id => user.id
     end
 
-
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
